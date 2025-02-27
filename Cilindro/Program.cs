57bc804ac70c80f413a884cddae62ff8a9d88b90
nunes <vera.ml.nunes@gmail.com>
2025-02-27 22:29:38 +0000

    Cilindro

---
+++ Cilindro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
