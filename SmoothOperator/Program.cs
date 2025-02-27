27cb04d48db9816d32defa7b311faafe7a76d546
nunes <vera.ml.nunes@gmail.com>
2025-02-27 21:59:51 +0000

    ex 1

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+           Console.WriteLine("Insere número inteiro: ");
+           sbyte n = sbyte.Parse(Console.ReadLine());
+
+           Console.WriteLine(--n);
+        
+           Console.WriteLine(++n);
+
+        }
+    }
+}
