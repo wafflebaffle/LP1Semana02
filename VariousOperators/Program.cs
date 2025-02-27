aced3145b6e9bdfcb745dbeee2b0bd20a81d3c10
nunes <vera.ml.nunes@gmail.com>
2025-02-27 22:24:38 +0000

    ex 2

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insere número inteiro-não negativo: ");
+            byte n = byte.Parse(Console.ReadLine());
+            
+            Console.WriteLine(n/2);
+            Console.WriteLine(n<<3);
+            Console.WriteLine(n^6);
+            Console.WriteLine(n>10);
+            
+        }
+    }
+}
