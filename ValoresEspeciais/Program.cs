8f911a7dd03f745a628a5832a82c01696fb92ded
nunes <vera.ml.nunes@gmail.com>
2025-02-28 13:33:40 +0000

    ValoresEspeciais

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
